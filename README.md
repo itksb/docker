42997bb3fc6b8ccd8c3854966afbe83627e6dd54
ksb <ksb@em70.ru>
2017-10-26 17:04:39 +0700

    pgadmin4

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # docker
 
-Docker-based tools and project skeletons
+Bash-wrapper for pgadmin4 docker-container
+
+### Running
+
+- git clone https://github.com/itksb/docker.git
+- cd docker
+- checkout pgadmin4
+- edit .env file
+- ./run.sh -s  - run service
+- open http://localhost:8050 (according to .env param) your browser
+- enjoy
---
+++ run.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+source .env
+
+print_help() {
+  echo "Information"
+  echo "Usage: ./run.sh options..."
+  echo "Options:"
+  echo "    -h  this help"
+  echo "    -s  start service"
+  echo "    -k  stop service"
+  echo "    -e  enter to the running container"
+  echo "    -p  service status"
+}
+
+
+start_service() {
+  echo "========== PGADMIN 4 ========="
+  echo "Open your browser: http://localhost:${APP_EXPOSE_PORT}"
+  docker run -d \
+  	-p${APP_EXPOSE_PORT}:80 \
+  	--name="${APP_CONTAINER_NAME}" \
+  	--rm \
+  	-e "PGADMIN_DEFAULT_EMAIL=${PGADMIN_DEFAULT_EMAIL}" \
+	-e "PGADMIN_DEFAULT_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}" \
+	dpage/pgadmin4
+}
+
+
+stop_service() {
+  docker kill "${APP_CONTAINER_NAME}"
+}
+
+
+exec_in_container() {
+  docker exec "${APP_CONTAINER_NAME}" bash
+}
+
+state() {
+  docker ps -f "name=${APP_CONTAINER_NAME}"
+}
+
+
+if [ $# = 0 ]; then
+    print_help
+fi
+
+
+while getopts ":hske:p" opt;
+do
+  case $opt in
+	h) print_help
+	;;
+	s) start_service
+	;;
+	k) stop_service
+	;;
+	e) exec_in_container
+	;;
+	p) state
+	;;
+	*) echo 'Wrong key! Run "run.sh -h" for help'
+	  exit 1
+	;;
+  esac
+done
